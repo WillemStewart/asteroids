fff370f615d3ec3d149cc22f6db84b7d3730871e
WillemStewart <dudeness64@sbcglobal.net>
2025-01-04 01:41:06 -0600

    Drew the little guy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
@@ -6,13 +7,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init
+    clock = pygame.time.Clock()
+    guy = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        guy.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
